4d2d7de2c636d170d69c2b1066fe5323a3dcf651
ibraheemashour <ibrahimashour.m@gmail.com>
2025-04-22 18:47:37 +0300

    style: fix indentation

--- bikeshare.py
+++ bikeshare.py
@@ -115,7 +115,7 @@ def display_data(df):
 
 def main():
     city, month, day = get_filters()
-    df = load_data(city, month)
+    df = load_data(city, month,day)
 
     time_stats(df)
     station_stats(df)
